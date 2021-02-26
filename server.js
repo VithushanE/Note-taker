97b100956ea11460b4354dc765b9b2ca6cdf13ac
Vithushan <vithushan@Vithushans-MacBook-Pro.local>
2021-02-26 18:46:12 -0500

    updated files

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const fs = require('fs') // need to install fs because we will be using a file system
 const app= express()
 
-const PORT = 8080
-const saveFile= '/.notetaker.json'
+const PORT = 8000
+
 
 // Step 2: get the apps ready
 // Have to use this to share the static files with the browser 
@@ -18,8 +18,7 @@ app.use(express.json())
 
 
 //Step 3: Now, we will be collecting the data from users and parsing the data in a JSON format 
-let getNotes = fs.existsSync(saveFile)? 
-JSON.parse(fs.readFileSync(saveFile)) : []
+const getNotes =JSON.parse(fs.readFileSync('notetaker.json'))
 
 
 //Step 4: we need the get, post and delete methods for the notes
@@ -28,35 +27,41 @@ app.get('/api/public/notes', function(req,res){
     console.log('getting files ')
 
     //always define where the date will be retrived 
-    let notesData = JSON.parse(fs.readFileSync('/.notetaker.json'))
+    const notesData = getNotes
    
     if (notesData) {
         res.send({notesData})
     }
 })
-s
+
 //2.) for users posting notes on page
+let counter = 1 
 app.post('/api/notes', function(req,res){
     console.log('posting notes on page', req.body)
 
-    let notesData= JSON.parse(fs.readFileSync('/.notetaker.json'))
+    let notesData= getNotes
 
-    //req.body means what i put in the body
-    const newNoteData= req.body
-    console.log(`[notes] notetaker(${getNotes.length} entries), adding newNotes: \n`, newNoteData)
-    getNotes.push(newNoteData)
+    if (notesData.length>0){
+        counter= (parseInt(notesData[notesData.length-1].id)+1)
+    }
+    else {
+        counter=1
+    }
+    notesData.push({id:`${counter}`, title: `${req.body.title}`, text: `${req.body.text}`})
+    fs.writeFileSync('notesData', JSON.stringify(notesData))    
 
+    counter++
+    req.location.reload()
+})
 
-    // Now we need to save the file as a string 
-    fs.writeFileSync(saveFile, JSON.stringify(saveNote))
-
-    res.send({message: `Reserved for *${newNoteData.name}*`})
-}); 
 
 
 app.delete('api/notes/:id', function(req,res){
     console.log('deleting entries')
-    let notesData = JSON.parse(fs.readFileSync('/.notetaker.json'))
+    let notesData = getNotess
+notesData = notesData.filter(entry => !(entry.id== req.params.id));
+fs.writeFileSync('notetaker.json', JSON.stringify(notesData));
+res.end();
 
 
 })
