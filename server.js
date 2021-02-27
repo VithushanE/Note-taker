46afdf0aef9d6d7e1e150ff067c282bce7b39f9c
Vithushan <vithushan@Vithushans-MacBook-Pro.local>
2021-02-27 00:27:04 -0500

    final changes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Step 1: set up all the required packages 
+const { prototype } = require('events')
 const express = require('express')
 const fs = require('fs') // need to install fs because we will be using a file system
-const app= express()
+const app = express()
 
 const PORT = 200
 
@@ -12,18 +13,12 @@ app.use(express.static('public'))
 
 // Now, we need to be able to accept incoming POST requests from the user
 
-// this is used to tell computer the information from user is parsed 
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
-//Step 3: Now, we will be collecting the data from users and parsing the data in a JSON format 
-const notesData =JSON.parse(fs.readFileSync('db/db.json'))
-console.log(notesData)
-
-//Step 4: we need the get, post and delete methods for the notes
-//1.) getting information shown on front page
-app.get('/api/notes', function(req,res){
+//Step 3: we need the get, post and delete methods for the notes
+//1.) getting information from the server db.json to be shown to mainpage
+app.get('/api/notes', function (req, res) {
     let notesData = JSON.parse(fs.readFileSync('db/db.json'))
     console.log('getting files ')
 
@@ -34,22 +29,23 @@ app.get('/api/notes', function(req,res){
 })
 
 //2.) for users posting notes on page
-let counter = 1 
-app.post('/api/notes', function(req,res){
+let counter = 1
+app.post('/api/notes', function (req, res) {
     console.log('posting notes on page', req.body)
 
     let notesData = JSON.parse(fs.readFileSync('db/db.json'))
-    if (notesData.length>0){
-        counter= (parseInt(notesData[notesData.length-1].id)+1)
+
+    if (notesData.length > 0) {
+        counter = (parseInt(notesData[notesData.length - 1].id) + 1)
     }
     else {
-        counter=1
+        counter = 1
     }
 
-    notesData.push({id:`${counter}`, title: `${req.body.title}`, text: `${req.body.text}`})
-    
+    notesData.push({ id: `${counter}`, title: `${req.body.title}`, text: `${req.body.text}` })
 
-    fs.writeFileSync('db/db.json', JSON.stringify(notesData))    
+
+    fs.writeFileSync('db/db.json', JSON.stringify(notesData))
 
     counter++
 })
@@ -57,14 +53,14 @@ app.post('/api/notes', function(req,res){
 
 
 app.delete('/api/notes/:id', (req, res) => {
-    console.log('deleting from page',`id:${req.params.id}`)
+    console.log('deleting from page', `id:${req.params.id}`)
     let data = JSON.parse(fs.readFileSync('db/db.json'));
     data = data.filter(entry => !(entry.id == req.params.id));
     fs.writeFileSync('db/db.json', JSON.stringify(data));
     res.end();
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
 
-        console.log('connecting to' + PORT)
+    console.log(`connecting to + ${PORT}`)
 })
